a4d077e3d6211ef37839bb4d9a4290ab57e24cc9
bafco123 <mohapibakuena6@gmail.com>
2025-11-02 21:06:08 +0000

    Update API base URL for production

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 const API = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000'
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'https://movie-backend-b8zz.onrender.com'
 });
 export default API;
